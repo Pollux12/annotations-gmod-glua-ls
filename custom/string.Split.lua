c1f6887f674216887364ab90327341ecd0936368
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-04-30 21:09:55 +0100

    Add override for string.Split

---
+++ custom/string.Split.lua
@@ -0,0 +1,10 @@
+---Splits the string into a table of strings, separated by the second argument.
+---
+--- This is an alias of [string.Explode](https://wiki.facepunch.com/gmod/string.Explode), but with flipped arguments.
+---@realm shared
+---@realm menu
+---@source https://wiki.facepunch.com/gmod/string.Split
+---@param Inputstring string String to split
+---@param Separator string Character(s) to split with.
+---@return string[] # Split table
+function string.Split(Inputstring, Separator) end

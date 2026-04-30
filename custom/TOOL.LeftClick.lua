72a3915829b225022ad014c51b3beed054154c48
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-04-30 02:07:21 +0100

    Add override for LeftClick and RightClick

---
+++ custom/TOOL.LeftClick.lua
@@ -0,0 +1,5 @@
+---Called when the player left clicks with the tool.
+---@realm shared
+---@source https://wiki.facepunch.com/gmod/TOOL:LeftClick
+---@return TraceResult
+function TOOL:LeftClick() end
---
+++ custom/TOOL.RightClick.lua
@@ -0,0 +1,5 @@
+---Called when the player right clicks with the tool.
+---@realm shared
+---@source https://wiki.facepunch.com/gmod/TOOL:RightClick
+---@return TraceResult
+function TOOL:RightClick() end

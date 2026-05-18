01a92dd24b96035852ba978ff3648d8de575b553
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-05-18 12:30:24 +0100

    Fix ColorAlpha color param

--- custom/Global.ColorAlpha.lua
+++ custom/Global.ColorAlpha.lua
@@ -2,7 +2,7 @@
 ---@realm shared
 ---@realm menu
 ---@source https://wiki.facepunch.com/gmod/Global.ColorAlpha
----@param color table The Color from which to take RGB values. This color will not be modified.
+---@param color Color The Color from which to take RGB values. This color will not be modified.
 ---@param alpha number The new alpha value, a number between 0 and 255. Values above 255 will be clamped.
 ---@return Color # The new Color with the modified alpha value
 function _G.ColorAlpha(color, alpha) end

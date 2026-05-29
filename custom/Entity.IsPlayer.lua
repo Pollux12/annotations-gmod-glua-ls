4f8e47392b76853466642e244068b273aec55d11
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-05-29 01:29:06 +0100

    Add IsPlayer override to narrow to player

---
+++ custom/Entity.IsPlayer.lua
@@ -0,0 +1,6 @@
+---Checks if the entity is a player or not.
+---@realm shared
+---@source https://wiki.facepunch.com/gmod/Entity:IsPlayer
+---@return boolean # Whether the entity is a player.
+---@return_cast self Player
+function Entity:IsPlayer() end

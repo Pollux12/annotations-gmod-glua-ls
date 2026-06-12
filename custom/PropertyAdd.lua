16e86a9f8a69073dcc328e56af338137102d2098
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-12 03:18:09 +0100

    Fix PropertyAdd optional fields

---
+++ custom/PropertyAdd.lua
@@ -0,0 +1,16 @@
+---Structure used for [properties.Add](https://wiki.facepunch.com/gmod/properties.Add).
+---@realm shared
+---@source https://wiki.facepunch.com/gmod/Structures/PropertyAdd
+---@class (partial) PropertyAdd
+---@field Type? string|"simple"|"toggle" Can be set to "toggle" to make this property a toggle property.
+---@field MenuLabel string Label to show on opened menu.
+---@field MenuIcon? string Icon to show on opened menu for this item. Optional for simple properties and unused for toggle properties.
+---@field Order number Where in the list this property should be positioned, relative to other properties.
+---@field PrependSpacer? boolean Whether to add a spacer before this property.
+---@field Filter fun(self: table, ent: Entity, player: Player):(check: boolean) Used clientside to decide whether this property should be shown for an entity.
+---@field Checked? fun(self: table, ent: Entity, tr: table):(check: boolean) Required only for toggle properties.
+---@field Action fun(self: table, ent: Entity, tr: table) Called clientside when the property is clicked.
+---@field Receive? fun(self: table, len: number, ply: Player) Called serverside if the client sends a message in the Action function.
+---@field MenuOpen? fun(self: table, option: DMenuOption, ent: Entity, tr: table) Called clientside when the property option has been created in the right-click menu.
+---@field OnCreate? fun(self: table, menu: DMenu, option: DMenuOption) Called clientside after the property option has been created.
+local PropertyAdd = {}

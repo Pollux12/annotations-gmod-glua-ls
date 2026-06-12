d7a0cf5aa8ade612805178abdadb8ffc0e09ef0c
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-12 03:03:17 +0100

    Add IsHostingGame menu annotation

---
+++ custom/Global.IsHostingGame.lua
@@ -0,0 +1,4 @@
+---Returns true when the current menu session is hosting a local game.
+---@realm menu
+---@return boolean #True if the local client is hosting the active game session.
+function _G.IsHostingGame() end

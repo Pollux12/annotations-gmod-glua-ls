bfb02540432a2c84437277cae9d0dda26d86df19
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-13 20:58:34 +0100

    Fix DListView line panel column annotations

---
+++ custom/DListView_Line.SetValue.lua
@@ -0,0 +1,8 @@
+---Alias of [DListView_Line:SetColumnText](https://wiki.facepunch.com/gmod/DListView_Line:SetColumnText).
+---@realm client
+---@realm menu
+---@source https://wiki.facepunch.com/gmod/DListView_Line:SetValue
+---@param column number The number of the column to write the value to, starts with 1.
+---@param value string|Panel Column text, or a panel to parent into the column.
+---@return DLabel? label The DLabel in which the text was set when `value` is a string.
+function DListView_Line:SetValue(column, value) end

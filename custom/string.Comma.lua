a51677b8b511beead478749661dbc373488a3382
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-13 23:56:27 +0100

    Add string.Comma and util.GetSunInfo override annotations

---
+++ custom/string.Comma.lua
@@ -0,0 +1,8 @@
+---Inserts commas for every third digit of a given number or numeric string.
+---@realm shared
+---@realm menu
+---@source https://wiki.facepunch.com/gmod/string.Comma
+---@param value number|string The input number or numeric string to commafy
+---@param separator? string An optional string that will be used instead of the default comma.
+---@return string # The commafied string
+function string.Comma(value, separator) end

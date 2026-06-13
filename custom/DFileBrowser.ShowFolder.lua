df3a9791886b584df4ee2c35466525054da50600
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-13 10:01:20 +0100

    Fix VGUI optional callback annotations

---
+++ custom/DFileBrowser.ShowFolder.lua
@@ -0,0 +1,8 @@
+---**INTERNAL**: This is used internally - although you're able to use it you probably shouldn't.
+--- Builds the file or icon list for the current directory.
+---
+--- You should use [DFileBrowser:SetCurrentFolder](https://wiki.facepunch.com/gmod/DFileBrowser:SetCurrentFolder) to change the directory.
+---@realm client
+---@source https://wiki.facepunch.com/gmod/DFileBrowser:ShowFolder
+---@param currentDir? string The directory to populate the list from.
+function DFileBrowser:ShowFolder(currentDir) end

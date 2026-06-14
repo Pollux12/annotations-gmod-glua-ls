220027ebf0aebbf0086335ca20307bad6d91d9ab
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-14 09:06:26 +0100

    Add DMenu:AddSpacer return override

---
+++ custom/DMenu.AddSpacer.lua
@@ -0,0 +1,6 @@
+---Adds a spacer to the DMenu.
+---@realm client
+---@realm menu
+---@source https://wiki.facepunch.com/gmod/DMenu:AddSpacer
+---@return (instance) DPanel #The created spacer panel.
+function DMenu:AddSpacer() end

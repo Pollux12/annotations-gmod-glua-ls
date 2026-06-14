3b2164dcc95919ba5806ff54acbf91013e932d2f
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-14 02:39:40 +0100

    Add Global.LoadPresets override annotation

---
+++ custom/Global.LoadPresets.lua
@@ -0,0 +1,9 @@
+---**INTERNAL**: This is used internally - although you're able to use it you probably shouldn't.
+---
+--- Loads all preset settings for the [presets](https://wiki.facepunch.com/gmod/presets) and returns them in a table
+---@realm client
+---@source https://wiki.facepunch.com/gmod/Global.LoadPresets
+---@class GmodPresets: table<string, table>
+---
+---@return GmodPresets # Preset data
+function _G.LoadPresets() end

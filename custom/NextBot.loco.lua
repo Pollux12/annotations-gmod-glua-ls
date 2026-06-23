8f3db74de97ff67523e4418ab2c71f34574c2319
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-23 01:05:40 +0100

    Add NextBot loco field annotation

---
+++ custom/NextBot.loco.lua
@@ -0,0 +1,7 @@
+---@meta
+
+--- The `CLuaLocomotion` instance that controls this NextBot's movement.
+-- Accessed via `self.loco` inside NextBot entity methods.
+---@class (partial) NextBot
+---@field loco CLuaLocomotion # The locomotion controller for this NextBot.
+local NextBot = {}

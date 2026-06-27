4aae9da1e2667d9d2e66648bf5a012c03d5480b5
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-06-27 02:47:15 +0100

    Add owner guard to deploy and think callbacks

---
+++ custom/WEAPON.Think.lua
@@ -0,0 +1,6 @@
+---Called when the weapon thinks.
+---@hook Think
+---@realm shared
+---@source https://wiki.facepunch.com/gmod/WEAPON:Think
+---@[self_call_valid("GetOwner")]
+function Weapon:Think() end

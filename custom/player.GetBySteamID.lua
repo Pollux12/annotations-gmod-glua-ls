667bf1a614ca066fc7f9225fdc13eda350f61acf
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-07-12 07:54:21 +0100

    Fix panel and player annotations

---
+++ custom/player.GetBySteamID.lua
@@ -0,0 +1,6 @@
+---Gets the player with the specified SteamID.
+---@realm shared
+---@source https://wiki.facepunch.com/gmod/player.GetBySteamID
+---@param steamID string The Player:SteamID to find the player by.
+---@return Player|false # Player if one is found, `false` otherwise.
+function player.GetBySteamID(steamID) end

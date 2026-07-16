92b73f0580ef25678a9ee7bb0d63ee12c87cc8fe
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-07-16 13:46:44 +0100

    Fix various annotations

---
+++ custom/DForm.ComboBox.lua
@@ -0,0 +1,9 @@
+---Adds a combo box to the form.
+---@realm client
+---@realm menu
+---@source https://wiki.facepunch.com/gmod/DForm:ComboBox
+---@param title string Text to the left of the combo box.
+---@param convar? string Console variable to change when the user selects something from the dropdown.
+---@return DComboBox # The created DComboBox
+---@return DLabel # The created DLabel
+function DForm:ComboBox(title, convar) end
